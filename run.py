d952c39ea59f91c4466814e4212f1a92647c0bbe
Nienke <poofcakes@hotmail.com>
2020-06-24 12:31:22 +0200

    WSGIMiddleware addition

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
+#!/usr/bin/python
+
+import sys
+
+sys.path.insert(0, '/')  # /data/vusualizer/VUSualizer
+
+
+class WSGIMiddleware(object):
+    def __init__(self, app, prefix):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = self.prefix
+        return self.app(environ, start_response)
+
+
 from src import app as application
+
+# Set any url prefix (script_root) here eg. /vusualizer
+application.wsgi_app = WSGIMiddleware(application.wsgi_app, "/")
 
 if __name__ == "__main__":
     application.run()
