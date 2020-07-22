652f6d3a360ed685469a31ed8737d9b39c040d29
Fini <31845711+FiniDG-HAN@users.noreply.github.com>
2020-07-22 16:04:18 +0200

    update progressbar
    updates progressbar to the new requirements.txt

--- extra/import_data.py
+++ extra/import_data.py
@@ -23,7 +23,7 @@ import io
 import time
 import pymongo
 import warnings
-import progressbar as p
+import progressbar
 from openpyxl import load_workbook
 
 
@@ -33,93 +33,95 @@ def main():
     db.drop()
 
     files = os.listdir("input")
-    bar = p.ProgressBar(maxval=len(files), \
-                        widgets=[p.Bar('=', '[', ']'), ' ', p.Percentage()])
-    print(len(files))
-    bar.start()
+    # bar = p.ProgressBar(maxval=len(files), \
+    #                    widgets=[p.Bar('=', '[', ']'), ' ', p.Percentage()])
+    # print(len(files))
+    # bar.start()
 
     # quiet the warning when loading the first xlsx
     warnings.simplefilter("ignore")
 
     start_time = time.time()
 
-    i = 0
-    for file in files:
-        i += 1
-        bar.update(i)
-        # To prevent opening a cached version of the file
-        if file.lower().endswith(".xlsx") and not file.lower().startswith("~"):
-            # print("#####\t\tParsing {}\t\t#####".format(file))
-            patient = {}
-            patient["dn_no"] = os.path.splitext(file)[0]
-            annotation = {}
-            data_headers = []
-            section = ""
+    with progressbar.ProgressBar(max_value=len(files)) as bar:
+        i = 0
+        for file in bar(files):
+            i += 1
+            # bar.update(i)
+            # To prevent opening a cached version of the file
+            if file.lower().endswith(".xlsx") and not file.lower().startswith("~"):
+                # print("#####\t\tParsing {}\t\t#####".format(file))
+                patient = {}
+                patient["dn_no"] = os.path.splitext(file)[0]
+                annotation = {}
+                data_headers = []
+                section = ""
 
-            with open("input/" + file, "rb") as f:
-                in_mem_file = io.BytesIO(f.read())
-            wb = load_workbook(in_mem_file)
+                with open("input/" + file, "rb") as f:
+                    in_mem_file = io.BytesIO(f.read())
+                wb = load_workbook(in_mem_file)
 
-            sheet = wb.active
-            for row in sheet:
+                sheet = wb.active
+                for row in sheet:
 
-                # First assess the section we're parsing
-                if section == "":
-                    # print("### Starting with parsing the Metadata section")
-                    section = "metadata"
-                    continue  # skip first row
-                elif row[0].value == "Annotation Sources":
-                    # print("### Detected switch to Annotation Sources section")
-                    section = "annotation"
-                elif section == "annotation" and row[2].value is None:
-                    # print("### Detected switch to Data Table section")
-                    patient["annotation_sources"] = annotation  # add previous section to total patient info
-                    section = "data_table"
-                    continue  # skip empty row
+                    # First assess the section we're parsing
+                    if section == "":
+                        # print("### Starting with parsing the Metadata section")
+                        section = "metadata"
+                        continue  # skip first row
+                    elif row[0].value == "Annotation Sources":
+                        # print("### Detected switch to Annotation Sources section")
+                        section = "annotation"
+                    elif section == "annotation" and row[2].value is None:
+                        # print("### Detected switch to Data Table section")
+                        patient["annotation_sources"] = annotation  # add previous section to total patient info
+                        section = "data_table"
+                        continue  # skip empty row
 
-                # Parse data based on current section
-                if section == "metadata":
-                    patient[row[0].value.replace(".", "").replace(" ", "_").lower()] = row[2].value
-                    pass
-                elif section == "annotation":
-                    annotation[row[2].value] = row[3].value
-                    pass
-                elif section == "data_table":
-                    if len(data_headers) == 0:  # get headers
-                        data_headers = [col.value for col in row]
-                    else:  # values
-                        variant = {}
-                        for i in range(len(data_headers)):  # iterate headers
-                            key = data_headers[i].replace(" ", "_").replace(".", "\uff0e").lower()
+                    # Parse data based on current section
+                    if section == "metadata":
+                        patient[row[0].value.replace(".", "").replace(" ", "_").lower()] = row[2].value
+                        pass
+                    elif section == "annotation":
+                        annotation[row[2].value] = row[3].value
+                        pass
+                    elif section == "data_table":
+                        if len(data_headers) == 0:  # get headers
+                            data_headers = [col.value for col in row]
+                        else:  # values
+                            variant = {}
+                            for i in range(len(data_headers)):  # iterate headers
+                                key = data_headers[i].replace(" ", "_").replace(".", "\uff0e").lower()
 
-                            # skip everything that starts with father or mother etc since the key
-                            # name contains patient number of parents
-                            # TODO: implement this properly
-                            if key.startswith("father") or key.startswith("mother") \
-                                or key.startswith("brother") or key.startswith("sister"):
-                                continue
+                                # skip everything that starts with father or mother etc since the key
+                                # name contains patient number of parents
+                                # TODO: implement this properly
+                                if key.startswith("father") or key.startswith("mother") \
+                                    or key.startswith("brother") or key.startswith("sister"):
+                                    continue
 
-                            # only use the name inbetween brackets to store, if last char is a right bracket
-                            if key.endswith(")"):
-                                index_bracket_left = key.rfind("(")
-                                if index_bracket_left >= 0:
-                                    key = key[index_bracket_left + 1:-1]
+                                # only use the name inbetween brackets to store, if last char is a right bracket
+                                if key.endswith(")"):
+                                    index_bracket_left = key.rfind("(")
+                                    if index_bracket_left >= 0:
+                                        key = key[index_bracket_left + 1:-1]
 
-                            val = row[i].value
-                            if key == "labels":
-                                labels = val.split(";")
-                                val = {}
-                                for label in labels:
-                                    labelInfo = label.split(",")
-                                    val[labelInfo[0].replace(" ", "_").replace(".", "\uff0e").lower()] = labelInfo[1]
-                            if val is not None and val != "":
-                                variant[key] = val
+                                val = row[i].value
+                                if key == "labels":
+                                    labels = val.split(";")
+                                    val = {}
+                                    for label in labels:
+                                        labelInfo = label.split(",")
+                                        val[labelInfo[0].replace(" ", "_").replace(".", "\uff0e").lower()] = labelInfo[1]
+                                if val is not None and val != "":
+                                    variant[key] = val
 
-                        # add variant info along with metadata to collection
-                        variant.update(patient)
-                        db.insert_one(variant)
-    print(i)
-    bar.finish()
+                            # add variant info along with metadata to collection
+                            variant.update(patient)
+                            db.insert_one(variant)
+            bar.update(bar.value)
+    #print(i)
+    #bar.finish()
     print("## \t\tFinished in {0:.2f} minutes".format((time.time() - start_time) / 60))
 
 
