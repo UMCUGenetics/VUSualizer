f43488a7dd9be8dcaa005b2d94a9fc24af228813
FiniDG-HAN <31845711+FiniDG-HAN@users.noreply.github.com>
2021-01-06 14:39:46 +0100

    copied from old vusualiser on dwergeik

---
+++ run_dwergeik.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.insert(0, '/data/vusualizer/VUSualizer')  # /data/vusualizer/VUSualizer
+
+
+class WSGIMiddleware(object):
+    def __init__(self, app, prefix):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        environ['SCRIPT_NAME'] = self.prefix
+        return self.app(environ, start_response)
+
+
+from src import app as application
+
+# Set any url prefix (script_root) here eg. /vusualizer
+application.wsgi_app = WSGIMiddleware(application.wsgi_app, "/")
+
+if __name__ == "__main__":
+    application.run()
