093b2e5b9787996ee6fa43f536c2bf458ee81fab
FiniDG-HAN <31845711+FiniDG-HAN@users.noreply.github.com>
2021-06-02 17:56:51 +0200

    added that only admin role can go to userview adminpage

--- src/models.py
+++ src/models.py
@@ -1,27 +1,27 @@
 import datetime
 import uuid
-from flask_login import UserMixin
+from flask import url_for, redirect, request
+from flask_login import UserMixin, current_user
 from flask_admin import Admin
 from flask_admin.contrib.pymongo import ModelView
+from flask_security import UserMixin
 from src import mongo
 import json
 from . import app
+from src import forms
 
 from wtforms import form, fields
 from flask_admin.contrib.pymongo import ModelView
-from flask_admin.model.fields import InlineFormField, InlineFieldList
 
 
 # User admin
-class InnerForm(form.Form):
-    name = fields.StringField('Name')
-    test = fields.StringField('Test')
-
 class User(UserMixin):
-    def __init__(self, email, password):
+    def __init__(self, email, password, role, active):
         self.id = email
         self.email = email
         self.password = password
+        self.active = active
+        self.role = role
 
     def is_authenticated(self):
         return True
@@ -34,13 +34,15 @@ class User(UserMixin):
 
     def get_id(self):
         return self.email
+    
+    def get_role(self):
+        return self.role
 
     @classmethod
     def get(cls, email):
         data = mongo.db.user.find_one({"email": email}, {"_id": 0})
         if data is not None:
-            return cls(email, data['password'])
-            # return cls(**data)
+            return cls(email, data['password'], data['role'], data['active'])
     
     # Required for administrative interface
     def __unicode__(self):
@@ -48,23 +50,39 @@ class User(UserMixin):
 
     def save_to_db(self):
         mongo.db.user.insert(self.__dict__)
+'''
+class Role(RoleMixin):
+    id = mongo.db.Column(mongo.db.Integer(), primary_key=True)
+    name = mongo.db.Column(mongo.db.String(80), unique=True, nullable=False)
+    description = mongo.db.Column(mongo.db.String(255))
+
+    def __repr__(self):
+        return "Role({0}-{1})".format(self.id, self.name)
+
+    def __str__(self):
+        return self.name
+'''
 
 # Customized admin views
 class UserForm(form.Form):
     name = fields.StringField('Name')
     email = fields.StringField('Email')
     password = fields.StringField('Password')
-
-    # Inner form
-    inner = InlineFormField(InnerForm)
-
-    # Form list
-    form_list = InlineFieldList(InlineFormField(InnerForm))
+    active = fields.BooleanField('Active')
+    role = fields.StringField('Role')
 
 class UserView(ModelView):
-    column_list = ('_id', 'email')
-    column_sortable_list = ('_id', 'email')
 
+    def is_accessible(self):
+        #return current_user.is_authenticated
+        return current_user.role == "ROLE_ADMIN"
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for('login', next=request.url))
+
+    column_list = ('_id', 'email', 'active', 'role')
+    column_sortable_list = ('_id', 'email', 'active', 'role')
     form = UserForm
 
 # Create admin
